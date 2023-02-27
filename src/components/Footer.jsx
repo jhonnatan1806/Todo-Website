89810fe883ca603df7a205d5f8783b8dc486ce4c
Jhonnatan E. Rojas <hytzy18@gmail.com>
2023-02-26 23:24:00 -0500

    header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-full flex flex-col items-center  gap-4">
+      <div className="w-full flex flex-row justify-evenly">
+        <a
+          href="https://github.com/Jhonnatan1806/frontend-todolist"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row items-center gap-2 px-4 py-2 rounded-md text-white bg-neutral-800"
+        >
+          <FaGithub size={30} />
+          <p> Frontend </p>
+        </a>
+        <a
+          href="https://github.com/Jhonnatan1806/backend-todolist"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row items-center gap-2 px-4 py-2 rounded-md text-white bg-neutral-800"
+        >
+          <FaGithub size={30} />
+          <p> Backend </p>
+        </a>
+      </div>
       <p>All rights reserved</p>
     </footer>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
   return (
     <>
       <header className="w-full inline-flex justify-center items-center gap-2">
-        <p className="font-bold text-4xl text-purple-500">TO-DO</p>
-        <p className="font-bold text-4xl text-neutral-800">LIST</p>
-        <FaCheck className="text-4xl text-neutral-800" />
+        <p className="font-bold text-4xl px-4 py-2 rounded-md text-white bg-purple-500 ">TO-DO</p>
+        <p className="font-bold text-4xl text-neutral-800 ">LIST</p>
+        <FaCheck className="text-4xl text-neutral-800 " />
       </header>
     </>
   )
